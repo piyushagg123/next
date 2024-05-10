8500f6a2e70cf5a628dad18c6089d142006c6344
piyushagg123 <piyushaggarwal20903@gmail.com>
2024-05-10 12:11:09 +0530

    added stickyScroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import HeroSection from "@/components/HeroSection";
 import FeaturedSection from "@/components/FeaturedSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
       <FeaturedSection />
+      <WhyChooseUs />
     </main>
   );
 }
